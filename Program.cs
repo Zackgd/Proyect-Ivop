9eb2f3da440cdaf3495e26bf555ee2c6d8fa068f
Zacarias Garcia <zacagarcia2330@gmail.com>
2025-05-23 22:37:33 -0300

    configuracion Pattern Repository

--- Program.cs
+++ Program.cs
@@ -1,7 +1,50 @@
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Cfg;
+using NHibernate;
+using System;
+using NHibernate.Tool.hbm2ddl;
+using Proyect_InvOperativa.Models;
+using Proyect_InvOperativa.Repository;
+using Proyect_InvOperativa.Mapping;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+
+builder.Services.AddSingleton(provider =>
+{
+    return Fluently.Configure()
+        .Database(
+            MySQLConfiguration.Standard
+                .ConnectionString("Server=localhost;Database=nombre_db;User=root;Password=tu_password;")
+                .ShowSql()
+        )
+        .Mappings(m => m.FluentMappings.AddFromAssemblyOf<ArticuloMapping>())
+        .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+        .BuildSessionFactory();
+});
+var sessionFactory = Fluently.Configure()
+    .Database(MsSqlConfiguration.MsSql2012
+        .ConnectionString(builder.Configuration.GetConnectionString("DefaultConnection"))
+        .ShowSql()
+    )
+    .Mappings(m => m.FluentMappings.AddFromAssembly(typeof(Program).Assembly))
+    .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+    .BuildSessionFactory();
+
+builder.Services.AddSingleton<ISessionFactory>(sessionFactory);
+builder.Services.AddScoped<NHibernate.ISession>(provider =>
+    provider.GetRequiredService<ISessionFactory>().OpenSession());
+
+// Registro de repositorios
+builder.Services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped<ArticuloRepository>();
+
+builder.Services.AddScoped<ArticuloRepository>();
 
 var app = builder.Build();
 
@@ -12,6 +55,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
---
+++ Repository/ArticuloRepository.cs
@@ -0,0 +1,16 @@
+﻿using NHibernate;
+using NHibernate.Linq;
+using Proyect_InvOperativa.Repository;
+using Proyect_InvOperativa.Models;
+
+namespace Proyect_InvOperativa.Repository
+{
+    public class ArticuloRepository : BaseRepository<Articulo>
+    {
+        public ArticuloRepository(ISessionFactory sessionFactory): base(sessionFactory) 
+        {
+        }
+
+    }
+}
+
