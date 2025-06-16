ad3b30ba0762c0bb69838a4c62993a975cc04d23
nico-agf33 <91765403+nico-agf33@users.noreply.github.com>
2025-06-15 22:13:26 -0300

    Update Proveedor.cs

--- Models/Proveedor.cs
+++ Models/Proveedor.cs
@@ -9,11 +9,8 @@
         public virtual string? telefono {  get; set; }
         public virtual MaestroArticulo? masterArticulo { get; set; }
         public virtual IList<ProveedorArticulo> proveedorArticulos { get; set; } = new List<ProveedorArticulo>();
-<<<<<<< HEAD
 
-=======
         public virtual bool predeterminado { get; set; }
->>>>>>> 2b87e89 (ajustes en modelos de stock)
 
     }
 }
